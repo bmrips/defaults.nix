6e1958f10e93d6d3f368927a50d4930b9430c376
Benedikt Rips <benedikt.rips@gmail.com>
2026-08-21 07:55:36 +0200

    feat(tex): integrate my build interface

--- modules/ecosystems/tex/default.nix
+++ modules/ecosystems/tex/default.nix
@@ -1,7 +1,43 @@
-{ config, lib, ... }:
-
 {
-  options.ecosystems.tex.enable = lib.mkEnableOption "tools for TeX development";
+  config,
+  lib,
+  pkgs,
+  self,
+  ...
+}:
+
+let
+  cfg = config.ecosystems.tex;
+in
+{
+  options.ecosystems.tex = {
+    enable = lib.mkEnableOption "tools for TeX development";
+    src = lib.mkOption {
+      description = ''
+        The source code for the documents.
+      '';
+      default = self;
+      defaultText = "self";
+      example = lib.literalExpression "./.";
+      type = lib.types.path;
+    };
+    packages = lib.mkOption {
+      description = ''
+        The packages to be installed in the TeX Live environment. It is a
+        function from the TeX Live package set to a list of packages.
+      '';
+      default = _: [ ];
+      defaultText = "(_: [])";
+      type = lib.types.anything;
+    };
+    documents = lib.mkOption {
+      description = ''
+        A derivation containing all documents that are built by the Makefile.
+      '';
+      type = lib.types.package;
+      readOnly = true;
+    };
+  };
 
   config = lib.mkIf config.ecosystems.tex.enable {
     git = {
@@ -15,5 +51,21 @@
     };
     pre-commit.settings.hooks.chktex.enable = true;
     treefmt.programs.latexindent.enable = true;
+
+    ecosystems.tex.documents = pkgs.stdenvNoCC.mkDerivation {
+      name = "documents";
+      inherit (cfg) src;
+      nativeBuildInputs = [ (pkgs.texliveBasic.withPackages cfg.packages) ];
+      preBuild = "export TEXMFVAR=$(mktemp -d)";
+    };
+
+    make-shells.default = {
+      inputsFrom = [ cfg.documents ];
+      shellHook = ''
+        TEXMFVAR=.cache/texmf-var
+        mkdir -p $TEXMFVAR
+        export TEXMFVAR="$(realpath "$TEXMFVAR")"
+      '';
+    };
   };
 }
