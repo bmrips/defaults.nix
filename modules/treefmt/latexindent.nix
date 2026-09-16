324f3149916e4431a8cc0f708ed243560ddae4da
Benedikt Rips <benedikt.rips@gmail.com>
2026-09-16 12:07:30 +0200

    fix(latexindent): ignore packages and document classes

--- modules/treefmt/latexindent.nix
+++ modules/treefmt/latexindent.nix
@@ -1,8 +1,24 @@
 { config, lib, ... }:
 
-lib.mkIf config.treefmt.programs.latexindent.enable {
-  git.ignore.${config.ecosystems.tex.root} = [
+let
+  # We can not wrap the top-level configuration with this condition since we
+  # would get an infinite recursion:
+  #  pre-commit hooks -> treefmt -> latexindent -> gitignore patterns ->
+  #  write-files pre-commit hook
+  ifEnabled = lib.mkIf config.treefmt.programs.latexindent.enable;
+in
+{
+  git.ignore.${config.ecosystems.tex.root} = ifEnabled [
     "*.bak*"
     "indent\.log"
   ];
+
+  # Do not format packages and document classes, only TeX documents and
+  # bibliography files.
+  treefmt.settings.formatter = ifEnabled {
+    latexindent.includes = lib.mkForce [
+      "*.tex"
+      "*.bib"
+    ];
+  };
 }
